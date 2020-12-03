04fa792407ae45d8314f5ef56b69a13ed91f9659
Leejoy <lprakash@assistcornerstone.com>
2020-12-03 20:49:11 +0530

    Added class to assign product to user

--- Backend/app.py
+++ Backend/app.py
@@ -3,11 +3,14 @@ from flask_restful import Api
 from flask_cors import CORS
 import firebase_admin
 from firebase_admin import credentials, firestore
+
 cred = credentials.Certificate("config/firebase_pvt_key.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+# It's necessary to import below class after initializing firebase
 from module.user import RegisterUser, UserLogin
+from module.product import Product
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,6 +18,7 @@ CORS(app)
 
 api.add_resource(UserLogin, '/login')
 api.add_resource(RegisterUser, '/register')
+api.add_resource(Product, '/product')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Backend/module/product.py
@@ -0,0 +1,50 @@
+from flask import jsonify
+from flask_restful import Resource, reqparse
+from bs4 import BeautifulSoup
+import urllib
+import urllib.request
+import re
+import datetime as dt
+from app import db
+
+parser = reqparse.RequestParser()
+
+
+class Product(Resource):
+
+    def post(self):
+        parser.add_argument('uid', type=str)
+        parser.add_argument('url', type=str)
+        args = parser.parse_args()
+
+        try:
+            if args['uid'] and args['url']:
+
+                with urllib.request.urlopen(args['url']) as response:
+                    html = response.read()
+
+                soup = BeautifulSoup(html, 'lxml')
+                price_tag = soup.find_all(id="priceblock_ourprice")
+                price_text = price_tag[0].get_text()
+                price_str = re.sub('([^0-9.])+', '', price_text)
+                price = float(price_str)
+
+                title_tag = soup.find_all('title')
+                title = title_tag[0].get_text()
+
+                if len(price_tag) > 0:
+                    doc_ref = db.collection('products')
+                    doc_ref.document().set({
+                        'dateAdded': dt.datetime.utcnow(),
+                        'url': args['url'],
+                        'uid': args['uid'],
+                        'initialPrice': price,
+                        'tile': title
+                    })
+                    return jsonify({'message': 'Product added successfully!', 'error': False, 'data': None})
+                else:
+                    return jsonify({'message': 'Unable to retrieve price', 'error': True, 'data': None})
+            else:
+                return jsonify({'message': 'Field missing', 'error': True, 'data': None})
+        except:
+            return jsonify({'message': 'Error while adding product', 'error': True, 'data': None})
