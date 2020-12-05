d88ac98ef0421d641621226a7335a37d30fecc63
Leejoy <lprakash@assistcornerstone.com>
2020-12-05 22:50:44 +0530

    Added scheduler to run job parallelly without affecting API

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restful import Api
+from flask_apscheduler import APScheduler
 from flask_cors import CORS
 import firebase_admin
 from firebase_admin import credentials, firestore
+import module.schedule_job as job
 
 cred = credentials.Certificate("config/firebase_pvt_key.json")
 firebase_admin.initialize_app(cred)
@@ -15,6 +17,11 @@ from module.product import Product
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
+
+scheduler.add_job(id='notification_job', func=job.product_price_notification, trigger='interval', seconds=60)
 
 api.add_resource(UserLogin, '/login')
 api.add_resource(RegisterUser, '/register')
